2f71301e6f842ae22cc1db3b89bd1b40b7e64a19
Kolla om vitesh <56569491+vitesh12@users.noreply.github.com>
2022-04-22 15:56:55 +0530

    FINAL

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:plant_app/screens/home/home_screen.dart';
 void main() {
   runApp(MyApp());
 }
-
+// main page
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
